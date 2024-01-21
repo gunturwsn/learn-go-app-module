23436f40b2a3b5ed5a6bdabaf8caf53b5d90a5b5
gunturwsn <gunturw79@gmail.com>
2024-01-21 11:55:56 +0700

    Day 13 learn go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gunturwsn/learn-go-app-module
+
+go 1.21.5
+
+require github.com/gunturwsn/learn-go-module/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	learn_go_module "github.com/gunturwsn/learn-go-module/v2"
+)
+
+func main() {
+	fmt.Println(learn_go_module.SayHello("Budi"))
+}
